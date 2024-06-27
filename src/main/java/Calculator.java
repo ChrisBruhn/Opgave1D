6f040a7b7714f790922466d4de8c7bb822f7e23b
ChrisBruhn <bruhn@eastcape.dk>
2024-06-27 16:45:17 +0200

    First

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,15 @@
+public class Calculator {
+
+
+
+    public int add (int numberA,
+             int numberB){
+
+        return numberA+numberB;
+    }
+
+    int multiply(int numberA, int numberB){
+        return numberA*numberB;
+    }
+
+}
